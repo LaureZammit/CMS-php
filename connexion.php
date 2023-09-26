93e5fb2b4b8c6302c982b2832d8c1ea1ac8c0499
Laure Zammit <zammitlaure@gmail.com>
2023-09-26 12:21:12 +0200

    Page profil connecte a la BDD

--- connexion.php
+++ connexion.php
@@ -4,6 +4,66 @@ session_start();
 // Pas d'erreur
 $errorMessage = '';
 
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+
+// Test si le formulaire est envoyé
+if(isset($_POST['email']) && isset($_POST['password'])) {
+    // Le formulaire à bien été envoyé
+
+    // On vérifie si les champs ne sont pas vides
+    if(!empty($_POST['email']) && !empty($_POST['password'])) {
+
+        // Si le mdp renseigné est inférieur à 6 caractères :
+        if(strlen($_POST['password']) < 6) {
+            $errorMessage = "Le mot de passe doit faire au moins 6 caractères<br/>";
+        }
+
+        if(strlen($_POST['password']) >= 6) {
+            // On fait appel à la BDD
+            require_once ('admin/connect.php');
+
+            // On récupère les données de l'utilisateur
+            $requete = "SELECT password_user as pass, id_user as id FROM users WHERE mail_user = :email";
+
+            // On prépare la requête
+            $requete = $db->prepare($requete);
+
+            // On injecte les valeurs
+            $requete->execute(array(
+                ':email' => $_POST['email']
+            ));
+
+            $result = $requete->fetch();
+
+            // Si on obtient pas de résultat : message d'erreur
+            if(!$result) {
+                $errorMessage = "Email ou mot de passe incorrect.<br/>";
+            }
+
+            // On teste si le mdp correspond
+            if($result) {
+                // Si mdp faux
+                if(!password_verify($_POST['password'], $result['pass'])) {
+                    $errorMessage = "Email ou mot de passe incorrect.<br/>";
+                }
+
+                if(password_verify($_POST['password'], $result['pass'])) {
+                    // Si on arrive ici : le mdp correspond
+
+                    $token = password_hash("Super connexion", PASSWORD_DEFAULT);
+
+                    // On connecte l'utilisateur avec les sessions
+                    $_SESSION['id'] = $result['id'];
+                    $_SESSION['token'] = $token;
+
+                    header('Location: profil.php');
+                }
+            }
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -20,14 +80,17 @@ $errorMessage = '';
         include_once 'components/header.php';
     ?>
     <main>
-        <h1>Page de connexion</h1>
-        <form action="profil.php" method="post">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Votre email" required>
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password" id="password" placeholder="Votre mot de passe" required>
-            <input type="submit" value="Se connecter">
-        </form>
+        <section class="connexion-membre">
+            <h1>Page de connexion</h1>
+            <form action="#" method="post">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Votre email" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" placeholder="Votre mot de passe" required>
+                <input class="submit-connexion" name="submit" type="submit" value="Se connecter">
+            </form>
+            <p class="errorMessage"><?= $errorMessage?></p>
+        </section>
     </main>
 </body>
 </html>
---
+++ profil.php
@@ -0,0 +1,98 @@
+<?php 
+session_start();
+
+// On vérifie si l'utilisateur possède des infos de session
+$session = isset($_SESSION["token"]) ? $_SESSION["token"] : false;
+
+$errorMessage = null;
+$prenom = null;
+$nom = null;
+$email = null;
+$pseudo = null;
+$password = null;
+$avatar = null;
+$compte = null;
+
+if($session) {
+
+    $token = $session;
+    $isTokenCorrect = password_verify("Super connexion", $token);
+
+    // On vérifie si le token est correct
+    if($isTokenCorrect) {
+        
+        // Mon token est correct je fais appel à ma base de données pour récupérer le mail
+        $id = isset($_COOKIE['id']) ? $_COOKIE['id'] : null;
+
+        // On fait appel à la BDD
+        require_once ('admin/connect.php');
+
+        // On récupère les données de l'utilisateur
+        $requete = "SELECT * FROM users WHERE id_user = :id";
+
+        // On prépare la requête
+        $requete = $db->prepare($requete);
+
+        // On injecte les valeurs
+        $requete->execute(array(
+            ':id' => $_SESSION['id']
+        ));
+
+        $result = $requete->fetch();
+
+        // On récupère les données de l'utilisateur
+        $prenom = $result['prenom_user'];
+        $nom = $result['nom_user'];
+        $email = $result['mail_user'];
+        $pseudo = $result['pseudo_user'];
+        $password = $result['password_user'];
+        $avatar = $result['avatar_user'];
+        $compte = $result['compte_user'];
+
+        if($result) {
+            // Si on arrive ici : l'utilisateur est connecté
+            $errorMessage = "Bienvenue " . $prenom . " " . $nom . "<br/>";
+        }
+
+        if(!$result) {
+            // Si on arrive ici : l'utilisateur n'est pas connecté
+            header('Location: connexion.php');
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Profil - <?=$prenom?></title>
+</head>
+<body>
+    <?php 
+        // Insertion du header.php
+        include_once 'components/header.php';
+    ?>
+
+    <main>
+        <section>
+            <h1><?=$errorMessage?></h1>
+    
+            <p>Votre profil</p>
+            <ul>
+                <li>Prénom : <?=$prenom?></li>
+                <li>Nom : <?=$nom?></li>
+                <li>Email : <?=$email?></li>
+                <li>Pseudo : <?=$pseudo?></li>
+                <li>Compte : <?=$compte?></li>
+                <li>Avatar : <?=$avatar?></li>
+            </ul>
+            <input type="submit" name="updateavatar" value="Modifier ma photo de profil">
+            <input type="submit" name="update" value="Modifier mes renseignements">
+            <input type="submit" name="delete" value="Supprimer mon compte">
+        </section>
+    </main>
+</body>
+</html>
